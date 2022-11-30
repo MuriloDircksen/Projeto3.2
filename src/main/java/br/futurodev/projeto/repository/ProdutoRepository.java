40935344a35a308eeac03324e882efef55782a29
Murilod <murilo.dircksen@gmail.com>
2022-11-30 16:45:08 -0300

    implmentação produto

--- src/main/java/br/futurodev/projeto/repository/ProdutoRepository.java
+++ src/main/java/br/futurodev/projeto/repository/ProdutoRepository.java
@@ -12,5 +12,6 @@ public interface ProdutoRepository extends CrudRepository<Produto, Long> {
 
     //@Query(value = "select p from PedidoModel p where p.clienteModel.nome like %?1%")
     //fazer querry que retorne todos os status true
+    @Query(value = "select SUM(p.precoProduto)from Produto p where p.statusProduto = COMPRADO")
     ArrayList<Produto> getProdutos();
 }
